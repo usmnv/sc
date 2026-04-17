24eb52cb604d5fdeb0dc7a4d2a01973c3443cb40
usmnv <usmannovv.l2@gmail.com>
2026-04-17 23:45:20 +0800

    Add files via upload

---
+++ data/init_db.sql
@@ -0,0 +1,8 @@
+-- Можно использовать для сброса БД
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS programs;
+DROP TABLE IF EXISTS universities;
+DROP TABLE IF EXISTS faq;
+DROP TABLE IF EXISTS guide_articles;
+
+-- Затем выполнить CREATE из database.py
